057e959b4b81271efc7939e2d0628f515e268390
Maikon Germano <mk_germano@hotmail.com>
2020-06-30 22:29:29 -0300

    updated at

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,7 @@
 const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+  Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value); // recebe o valor e formata pelo number format
 
 export default formatValue;
