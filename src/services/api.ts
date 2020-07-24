d2ed1b1d98a601551bc0ab9a1b0a8be0569beca5
Maikon Germano <mk_germano@hotmail.com>
2020-07-24 14:32:00 -0300

    updated connection

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://3.21.227.135:3333',//'http://localhost:3333',
 });
 
 export default api;
